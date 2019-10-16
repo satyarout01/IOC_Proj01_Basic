cab86559f4ec691362d6f5be5c5bc48c66cd5db3
SATYA <SATYA@DESKTOP-4F4CFAG>
2019-10-17 01:19:08 +0530

    1st Commit

---
+++ src/main/java/com/sr/test/App.java
@@ -0,0 +1,13 @@
+package com.sr.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/sr/test/AppTest.java
@@ -0,0 +1,9 @@
+package com.sr.test;
+
+class AppTest{
+	
+	public static void main(String[] args) {
+		System.out.println("AppTest.main()");
+	}
+	
+}
